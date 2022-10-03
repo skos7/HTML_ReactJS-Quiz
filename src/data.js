34e8fe312fe541fdd6a9d02d57d55bf98c7245f6
Sandi Kos <sandi.7kos@gmail.com>
2022-10-03 20:02:42 +0200

    HTML_ReactJS-Quiz
    Simple quiz with React hooks

---
+++ src/data.js
@@ -0,0 +1,143 @@
+const data = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        incorrectAnswers: [
+            "<scripting>",
+            "<js>",
+            "<javascript>",
+        ],
+        correctAnswer: "<script>",
+    },
+
+    {
+        question: "What is the correct command to create a new React project?",
+        incorrectAnswers: [
+            'npm create-react-app myReactApp',
+            'npx create-react-app',
+        ],
+        correctAnswer: 'npx create-react-app myReactApp',
+    },
+
+    {
+        question: "What does HTML stand for?",
+        incorrectAnswers: [
+           "Hyperlinks and Text Markup Language",
+           "Home Tool Markup Language",            
+        ],
+        correctAnswer: "Hyper Text Markup Language",
+    },
+
+    {
+        question: "How do you create a function in JavaScript?",
+        incorrectAnswers: [
+            "function = myFunction()",
+            "function:myFunction()",
+        ],
+        correctAnswer: "function myFunction()",
+    },
+
+    {
+        question: 'What command is used to start the React local development server?',
+        incorrectAnswers: [
+            "npm run dev",
+            "npm serve",
+            "npm build",
+        ],
+        correctAnswer: "npm start",
+    },
+
+    {
+        question: "Which character is used to indicate an end tag?",
+        incorrectAnswers: [
+           "^",
+           "<",   
+           "*",         
+        ],
+        correctAnswer: "/",
+    },
+
+    {
+        question: "How can you add a comment in a JavaScript?",
+        incorrectAnswers: [
+           "<!--This is a comment-->",
+           "'This is a comment",
+        ],
+        correctAnswer: "//This is a comment",
+    },
+
+    {
+        question: "What is the correct HTML element for inserting a line break?",
+        incorrectAnswers: [
+           '<break>',
+           '<lb>',
+        ],
+        correctAnswer: '<br>',
+    },
+
+    {
+        question: "JavaScript is the same as Java.",
+        incorrectAnswers: [
+           "True",
+        ],
+        correctAnswer: "False",
+    },
+
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        incorrectAnswers: [
+           "onchange",
+           "onmouseover",
+           "onmouseclick",  
+        ],
+        correctAnswer: "onclick",
+    },
+
+    {
+        question: "What is the default local host port that a React development server uses?",
+        incorrectAnswers: [
+           "5000",
+           "3500",
+           "8080",
+        ],
+        correctAnswer: "3000",
+    },
+
+    {
+        question: "In HTML, which attribute is used to specify that an input field must be filled out?",
+        incorrectAnswers: [
+           "formvalidate",
+           'placeholder',
+           'validate',
+        ],
+        correctAnswer: 'required',
+    },
+
+    {
+        question: "What is the correct syntax to import a Component from React?",
+        incorrectAnswers: [
+           "import [ Component ] from 'react'",
+           "import Component from 'react'",
+           "import React.Component from 'react'",
+        ],
+        correctAnswer: "import { Component } from 'react'",
+    },
+
+    {
+        question: "Which HTML element defines navigation links?",
+        incorrectAnswers: [
+           "<navigation>",
+           "<navigate>",
+        ],
+        correctAnswer: "<nav>",
+    },
+
+    {
+        question: "Is JavaScript case-sensitive?",
+        incorrectAnswers: [
+           "No",
+        ],
+        correctAnswer: "Yes",
+    },
+];
+
+export default data;
